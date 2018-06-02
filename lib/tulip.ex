74a74f5b804d934daa1ea1936583cfa4fff91b5f
hswick <hdswick@gmail.com>
2018-06-02 18:32:21 +0200

    Created unit tests for contract deployment and artifact retrieval

--- lib/tulip.ex
+++ lib/tulip.ex
@@ -1,15 +1,29 @@
 defmodule Tulip do
 
-  def deploy_contract(atom, args, options) do
+  def load_contract_artifacts(name, options) do
+    {abi_path, bin_path} =
+    if options[:build_path] do
+      path = "#{options[:build_path]}#{name}"
+      {"#{path}.abi", "#{path}.bin"}
+    else
+      {"./build#{name}.abi", "./build/#{name}.bin"}
+    end
+    
+    abi = ExW3.load_abi abi_path
+    bin = ExW3.load_bin bin_path
+
+    {abi, bin}
+  end
+
+  def deploy_contract(atom, args, options, tulip_options \\ []) do
 
     contract_name =
       atom
       |> Atom.to_string
       |> String.split(".")
-      |> Enum.at(1)	
-    
-    abi = ExW3.load_abi "./build/#{contract_name}.abi"
-    bin = ExW3.load_bin "./build/#{contract_name}.bin"
+      |> Enum.at(1)
+
+    {abi, bin} = load_contract_artifacts(contract_name, tulip_options)
     
     ExW3.Contract.start_link(atom, abi: abi)
     
@@ -26,6 +40,7 @@ defmodule Tulip do
         address = 
           ExW3.Contract.address(arg) 
           |> ExW3.format_address
+	address
       else
 	arg
       end
@@ -41,48 +56,67 @@ defmodule Tulip do
     end
   end
 
-  def deploy_contracts(contract_configs) do
+  def deploy_contracts(contract_configs, tulip_options) do
 
     accounts = ExW3.accounts
+    default_config = %{from: Enum.at(accounts, 0), gas: 300_000}
     
     name_address_tuples =
       Enum.map(contract_configs, fn config ->
 	{atom, args, options} = parse_config(config)
-	default_config = %{from: Enum.at(accounts, 0), gas: 300_000}
-	deploy_contract(atom, translate_args(args), Map.merge(default_config, Enum.into(options, %{})))
+	deploy_contract(
+	  atom,
+	  translate_args(args),
+	  Map.merge(default_config, Enum.into(options, %{})),
+	  tulip_options
+	)
       end)
 
     input = System.argv()
 
     network =
-    if input == [] do
+    if input == [] || tulip_options[:test] do
       "development"
     else
       Enum.at(input, 0)
     end
 
+    build_path =
+    if tulip_options[:build_path] do
+      tulip_options[:build_path]
+    else
+      "./build/"
+    end
+
     File.write(
-      "./build/#{network}.json",
+      "#{build_path}#{network}.json",
       name_address_tuples
       |> Enum.into(%{})
       |> Poison.encode!
     )
   end
 
-  def init_artifacts_context(network) do
-    addresses = Poison.decode!(File.read!("./build/#{network}.json"))
+  def init_artifacts_context(network, options) do
+    build_path =
+    if options[:build_path] do
+      options[:build_path]
+    else
+      "./build/"
+    end
+    
+    addresses = Poison.decode!(File.read!("#{build_path}#{network}.json"))
 
     contracts = 
       Enum.map(addresses, fn {contract_name, address} ->
-        abi = ExW3.load_abi("./build/#{contract_name}.abi")
-        bin = ExW3.load_bin("./build/#{contract_name}.bin")
+        abi = ExW3.load_abi("#{build_path}#{contract_name}.abi")
+        bin = ExW3.load_bin("#{build_path}#{contract_name}.bin")
         {String.to_atom(contract_name), %{address: address, abi: abi, bin: bin}}
       end)
     
-    Map.merge(%{network: network, accounts: ExW3.accounts()}, Enum.into(contracts, %{}))  
+    Map.merge(%{network: network, accounts: ExW3.accounts()}, Enum.into(contracts, %{}))
   end
   
-  def get_artifacts(is_test \\ false) do
+  def get_artifacts(options) do
 
     input = System.argv()
 
@@ -90,13 +124,13 @@ defmodule Tulip do
     if Kernel.length(input) == 1 do
       "development"
     else
-      if is_test do
+      if options[:test] do
         Enum.at(input, 1)
       else
         Enum.at(input, 0)
       end
     end
     
-    init_artifacts_context(network)
+    init_artifacts_context(network, options)
   end
 end
--- test/tulip_test.exs
+++ test/tulip_test.exs
@@ -1,6 +1,40 @@
 defmodule TulipTest do
   use ExUnit.Case
   doctest Tulip
-  test "greets the world" do
+  
+  test "load contract artifacts" do
+    {abi, bin} = Tulip.load_contract_artifacts("SimpleStorage", build_path: "./test/build/")
+
+    assert abi |> is_map
+    assert bin |> is_binary
   end
+
+  test "deploys contract" do
+    {name, address} = Tulip.deploy_contract(
+      SimpleStorage,
+      [],
+      [from: Enum.at(ExW3.accounts, 0), gas: 300_000],
+      build_path: "./test/build/"
+    )
+
+    assert name |> is_binary
+    assert address |> is_binary
+
+  end
+
+  test "deploys contracts and gets artifacts" do
+    result = Tulip.deploy_contracts([SimpleStorage], build_path: "./test/build/", test: true)
+    assert(result == :ok)
+
+    artifacts = Tulip.get_artifacts(build_path: "./test/build/")
+
+    assert artifacts[:SimpleStorage] |> is_map
+    assert artifacts[:SimpleStorage][:abi] |> is_map
+    assert artifacts[:SimpleStorage][:bin] |> is_binary
+    assert artifacts[:SimpleStorage][:address] |> is_binary
+    assert artifacts[:accounts] |> is_list
+    assert artifacts[:network] |> is_binary
+
+  end
+
 end
